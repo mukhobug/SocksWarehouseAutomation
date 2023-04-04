0c0e1e0319a35b0262f629a5ea89f4d4d67588b3
Alejandro <mukhobug@gmail.com>
2023-04-04 10:32:42 +0300

    Initial commit

---
+++ src/main/java/com/example/sockswarehouseautomation/SocksWarehouseAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.sockswarehouseautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksWarehouseAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksWarehouseAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sockswarehouseautomation/SocksWarehouseAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sockswarehouseautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksWarehouseAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
